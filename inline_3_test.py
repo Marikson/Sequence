95b1231b1d2b7d27d31234fe87eb58c15c6c833c
Andras Marik <andras.marik@tttech.com>
2026-02-28 23:29:24 +0100

    Inline 3 test DownLeft done

--- inline_3_test.py
+++ inline_3_test.py
@@ -1353,7 +1353,7 @@ class Inline3TestUpRight(unittest.TestCase):
         r, c = test_game.pick_cell()
         test_game.model.set_color("Red")
         for i in range(3, 5):
-            test_game.model.clicked_cell = {"r": 7-i, "c": i}
+            test_game.model.clicked_cell = {"r": i, "c": 7-i}
             r, c = test_game.pick_cell()
             test_game.model.set_color("Red")
         test_game.model.check_inline_per_color("Red", r, c)
@@ -1416,7 +1416,7 @@ class Inline3TestUpRight(unittest.TestCase):
 
         for i in range(1, 6):
             if i % 2 == 1:
-                test_game.model.clicked_cell = {"r": 7-i, "c": i}
+                test_game.model.clicked_cell = {"r": i, "c": 7-i}
                 r, c = test_game.pick_cell()
                 test_game.model.set_color("Red")
         test_game.model.check_inline_per_color("Red", r, c)
@@ -1428,8 +1428,242 @@ class Inline3TestUpRight(unittest.TestCase):
 
 
 class Inline3TestDownLeft(unittest.TestCase):
-     pass
-    
+    def test_0_empty_one_ended(self):
+        test_field = tk.Tk()
+        test_field.title("Test Field")
+
+        test_game = Sequence()
+        test_game.create_grid(test_field)
+
+        for i in range(8, 6, -1):
+            test_game.model.clicked_cell = {"r": i, "c": 9-i}
+            r, c = test_game.pick_cell()
+            test_game.model.set_color("Red")
+        test_game.model.check_inline_per_color("Red", r, c)
+        self.assertEqual(test_game.model.inline_dict["Red"]["inline"], 3)
+        self.assertEqual(test_game.model.inline_dict["Red"]["two_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["one_ended"], True)
+        self.assertEqual(test_game.model.inline_dict["Red"]["open_in_middle"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["empty_middle_counter"], 0)
+
+    def test_1_empty_one_ended(self):
+        test_field = tk.Tk()
+        test_field.title("Test Field")
+
+        test_game = Sequence()
+        test_game.create_grid(test_field)
+
+        for i in range(7, 5, -1):
+            test_game.model.clicked_cell = {"r": i, "c": 9-i}
+            r, c = test_game.pick_cell()
+            test_game.model.set_color("Red")
+        test_game.model.check_inline_per_color("Red", r, c)
+        self.assertEqual(test_game.model.inline_dict["Red"]["inline"], 3)
+        self.assertEqual(test_game.model.inline_dict["Red"]["two_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["one_ended"], True)
+        self.assertEqual(test_game.model.inline_dict["Red"]["open_in_middle"], True)
+        self.assertEqual(test_game.model.inline_dict["Red"]["empty_middle_counter"], 1)
+
+    def test_2_empty_only_middle(self):
+        test_field = tk.Tk()
+        test_field.title("Test Field")
+
+        test_game = Sequence()
+        test_game.create_grid(test_field)
+
+        for i in range(6, 4, -1):
+            test_game.model.clicked_cell = {"r": i, "c": 9-i}
+            r, c = test_game.pick_cell()
+            test_game.model.set_color("Red")
+        test_game.model.check_inline_per_color("Red", r, c)
+        self.assertEqual(test_game.model.inline_dict["Red"]["inline"], 3)
+        self.assertEqual(test_game.model.inline_dict["Red"]["two_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["one_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["open_in_middle"], True)
+        self.assertEqual(test_game.model.inline_dict["Red"]["empty_middle_counter"], 2)
+
+    def test_2_empty_only_middle_mirrored(self):
+        test_field = tk.Tk()
+        test_field.title("Test Field")
+
+        test_game = Sequence()
+        test_game.create_grid(test_field)
+
+        test_game.model.clicked_cell = {"r": 8, "c": 1}
+        r, c = test_game.pick_cell()
+        test_game.model.set_color("Red")
+        test_game.model.clicked_cell = {"r": 5, "c": 4}
+        r, c = test_game.pick_cell()
+        test_game.model.set_color("Red")
+        test_game.model.check_inline_per_color("Red", r, c)
+        self.assertEqual(test_game.model.inline_dict["Red"]["inline"], 3)
+        self.assertEqual(test_game.model.inline_dict["Red"]["two_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["one_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["open_in_middle"], True)
+        self.assertEqual(test_game.model.inline_dict["Red"]["empty_middle_counter"], 2)
+
+    def test_2_empty_only_middle_on_even_fields(self):
+        test_field = tk.Tk()
+        test_field.title("Test Field")
+
+        test_game = Sequence()
+        test_game.create_grid(test_field)
+
+        for i in range(8, 5, -1):
+            if i % 2 == 0:
+                test_game.model.clicked_cell = {"r": i, "c": 9-i}
+                r, c = test_game.pick_cell()
+                test_game.model.set_color("Red")
+        test_game.model.check_inline_per_color("Red", r, c)
+        self.assertEqual(test_game.model.inline_dict["Red"]["inline"], 3)
+        self.assertEqual(test_game.model.inline_dict["Red"]["two_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["one_ended"], True)
+        self.assertEqual(test_game.model.inline_dict["Red"]["open_in_middle"], True)
+        self.assertEqual(test_game.model.inline_dict["Red"]["empty_middle_counter"], 1)
+
+    def test_2_empty_only_middle_on_odd_fields(self):
+        test_field = tk.Tk()
+        test_field.title("Test Field")
+
+        test_game = Sequence()
+        test_game.create_grid(test_field)
+
+        for i in range(8, 4, -1):
+            if i % 2 == 1:
+                test_game.model.clicked_cell = {"r": i, "c": 9-i}
+                r, c = test_game.pick_cell()
+                test_game.model.set_color("Red")
+        test_game.model.check_inline_per_color("Red", r, c)
+        self.assertEqual(test_game.model.inline_dict["Red"]["inline"], 3)
+        self.assertEqual(test_game.model.inline_dict["Red"]["two_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["one_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["open_in_middle"], True)
+        self.assertEqual(test_game.model.inline_dict["Red"]["empty_middle_counter"], 2)
+
+
+    def test_0_empty_two_ended(self):
+        test_field = tk.Tk()
+        test_field.title("Test Field")
+
+        test_game = Sequence()
+        test_game.create_grid(test_field)
+
+        for i in range(1, 4):
+            test_game.model.clicked_cell = {"r": 7-i, "c": i}
+            r, c = test_game.pick_cell()
+            test_game.model.set_color("Red")
+        test_game.model.check_inline_per_color("Red", r, c)
+        self.assertEqual(test_game.model.inline_dict["Red"]["inline"], 3)
+        self.assertEqual(test_game.model.inline_dict["Red"]["two_ended"], True)
+        self.assertEqual(test_game.model.inline_dict["Red"]["one_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["open_in_middle"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["empty_middle_counter"], 0)
+
+    def test_1_empty_two_ended(self):
+        test_field = tk.Tk()
+        test_field.title("Test Field")
+
+        test_game = Sequence()
+        test_game.create_grid(test_field)
+
+        for i in range(1, 3):
+            test_game.model.clicked_cell = {"r": 7-i, "c": i}
+            r, c = test_game.pick_cell()
+            test_game.model.set_color("Red")
+        test_game.model.clicked_cell = {"r": 3, "c": 4}
+        r, c = test_game.pick_cell()
+        test_game.model.set_color("Red")
+        test_game.model.check_inline_per_color("Red", r, c)
+        self.assertEqual(test_game.model.inline_dict["Red"]["inline"], 3)
+        self.assertEqual(test_game.model.inline_dict["Red"]["two_ended"], True)
+        self.assertEqual(test_game.model.inline_dict["Red"]["one_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["open_in_middle"], True)
+        self.assertEqual(test_game.model.inline_dict["Red"]["empty_middle_counter"], 1)
+
+    def test_1_empty_two_ended_mirrored(self):
+        test_field = tk.Tk()
+        test_field.title("Test Field")
+
+        test_game = Sequence()
+        test_game.create_grid(test_field)
+
+        test_game.model.clicked_cell = {"r": 6, "c": 1}
+        r, c = test_game.pick_cell()
+        test_game.model.set_color("Red")
+        for i in range(3, 5):
+            test_game.model.clicked_cell = {"r": 7-i, "c": i}
+            r, c = test_game.pick_cell()
+            test_game.model.set_color("Red")
+        test_game.model.check_inline_per_color("Red", r, c)
+        self.assertEqual(test_game.model.inline_dict["Red"]["inline"], 3)
+        self.assertEqual(test_game.model.inline_dict["Red"]["two_ended"], True)
+        self.assertEqual(test_game.model.inline_dict["Red"]["one_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["open_in_middle"], True)
+        self.assertEqual(test_game.model.inline_dict["Red"]["empty_middle_counter"], 1)
+
+    def test_2_empty_only_middle_NOT_two_ended(self):
+        test_field = tk.Tk()
+        test_field.title("Test Field")
+
+        test_game = Sequence()
+        test_game.create_grid(test_field)
+
+        for i in range(1, 3):
+            test_game.model.clicked_cell = {"r": 7-i, "c": i}
+            r, c = test_game.pick_cell()
+            test_game.model.set_color("Red")
+        test_game.model.clicked_cell = {"r": 2, "c": 5}
+        r, c = test_game.pick_cell()
+        test_game.model.set_color("Red")
+        test_game.model.check_inline_per_color("Red", r, c)
+        self.assertEqual(test_game.model.inline_dict["Red"]["inline"], 3)
+        self.assertEqual(test_game.model.inline_dict["Red"]["two_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["one_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["open_in_middle"], True)
+        self.assertEqual(test_game.model.inline_dict["Red"]["empty_middle_counter"], 2)
+
+
+    def test_2_empty_only_middle_NOT_two_ended_mirrored(self):
+        test_field = tk.Tk()
+        test_field.title("Test Field")
+
+        test_game = Sequence()
+        test_game.create_grid(test_field)
+
+        test_game.model.clicked_cell = {"r": 6, "c": 1}
+        r, c = test_game.pick_cell()
+        test_game.model.set_color("Red")
+        for i in range(4, 6):
+            test_game.model.clicked_cell = {"r": 7-i, "c": i}
+            r, c = test_game.pick_cell()
+            test_game.model.set_color("Red")
+        
+        test_game.model.check_inline_per_color("Red", r, c)
+        self.assertEqual(test_game.model.inline_dict["Red"]["inline"], 3)
+        self.assertEqual(test_game.model.inline_dict["Red"]["two_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["one_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["open_in_middle"], True)
+        self.assertEqual(test_game.model.inline_dict["Red"]["empty_middle_counter"], 2)
+
+    def test_chess_pattern_NOT_two_ended(self):
+        test_field = tk.Tk()
+        test_field.title("Test Field")
+
+        test_game = Sequence()
+        test_game.create_grid(test_field)
+
+        for i in range(1, 6):
+            if i % 2 == 1:
+                test_game.model.clicked_cell = {"r": 7-i, "c": i}
+                r, c = test_game.pick_cell()
+                test_game.model.set_color("Red")
+        test_game.model.check_inline_per_color("Red", r, c)
+        self.assertEqual(test_game.model.inline_dict["Red"]["inline"], 3)
+        self.assertEqual(test_game.model.inline_dict["Red"]["two_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["one_ended"], False)
+        self.assertEqual(test_game.model.inline_dict["Red"]["open_in_middle"], True)
+        self.assertEqual(test_game.model.inline_dict["Red"]["empty_middle_counter"], 2)
+         
 
 class Inline3TestDownRight(unittest.TestCase):
      pass
